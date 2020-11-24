881e5ac31efa76043827ef4012db8c24fc8c0412
gosimitz <12478390+gosimitz@users.noreply.github.com>
2020-11-23 21:23:12 -0700

    Implemented the file downloader in BTSDownload, added a .properties file handler, and mapped out future steps.

--- src/BTSDownload.java
+++ src/BTSDownload.java
@@ -1,5 +1,44 @@
-public class BTSDownload {
-    public static void sayHi(String greeting) {
-        System.out.println(greeting);
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+
+/**
+ * A simple class to download a Ontime Carrier performance file from the transtats.bts.gov website
+ */
+class BTSDownload {
+    /**
+     * Adapted from: https://stackabuse.com/how-to-download-a-file-from-a-url-in-java/
+     * A method to download a file using the schema below.
+     * https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_ + YEAR + _ + MONTH + .zip
+     * @param filePathInSystem - /Users/User/Desktop/ - where you wish the file to be stored.
+     * @param year - year of data desired (YYYY)
+     * @param month - month of data requested (no leading zeros)
+     * @return a relative file path to the file downloaded as a string.
+     */
+    static String downloadFile(String filePathInSystem, int year, int month) {
+        try {
+            ReadableByteChannel readChannel = Channels.newChannel(new URL("https://transtats.bts.gov/PREZIP/On_Time_" +
+                    "Reporting_Carrier_On_Time_Performance_1987_present_" + year + "_" + month + ".zip").openStream());
+            FileOutputStream fileOS = new FileOutputStream(filePathInSystem + month + "-" + year + ".zip");
+            FileChannel writeChannel = fileOS.getChannel();
+            writeChannel.transferFrom(readChannel, 0, Long.MAX_VALUE);
+        }
+        catch(Exception e){
+            return "-1";
+        }
+
+        return "";
+    }
+
+    /**
+     * Delets filePath when called.
+     * @param filePath - Filepath of the file to delete
+     * @return True if successfully deleted, False if not found or error when delete attempted.
+     */
+    static boolean deleteFile(String filePath) {
+        //#TODO Implement me!
+        return false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
-        BTSDownload.sayHi("All Set");
+        Properties properties = getProperties("src/config.properties");
+        String filepath = properties.getProperty("filepath");
+        BTSDownload.downloadFile(filepath, 2020, 2);
+        //unzip
+        //create data.world project
+        //upload files from unzip
+        //delete files from unzip
+        //move on.
+    }
+
+    /**
+     * A method to read in a properties file.
+     * Adapted from: https://www.codejava.net/coding/reading-and-writing-configuration-for-java-application-using-properties-class
+     * @param propFileName - filepath of properties file.
+     * @return the Properties object made from the properties file.
+     * or a new Properties object if there is an error in accessing properties
+     */
+    private static Properties getProperties(String propFileName) {
+        File configFile = new File(propFileName);
+
+        try {
+            FileReader reader = new FileReader(configFile);
+            Properties props = new Properties();
+            props.load(reader);
+            return props;
+
+        } catch (Exception ex) {
+            // file does not exist
+        }
+        return new Properties();
     }
 }
